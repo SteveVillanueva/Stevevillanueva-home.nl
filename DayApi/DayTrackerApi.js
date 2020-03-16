e1e19b02393819c6097f9cdd02f9b48dae712897
Steve Villanueva <steve.villanueva@edu-kw1c.nl>
2020-03-16 16:02:10 +0100

    updated post request

--- DayApi/DayTrackerApi.js
+++ DayApi/DayTrackerApi.js
@@ -20,61 +20,47 @@ db.once('open', _ => {
 db.on('error', err => {
   console.error('connection error:', err)
 })
+
 // schema
 const ratingSchema = new mongoose.Schema({
-  name: String,
   // key
   date: { type: Date, required: true, unique: true },
   rating: { type: Number, required: true },
   comment: String
 })
 
-var Rating = mongoose.model('Rating', ratingSchema)
+// model
+var Rating = mongoose.model('Rating', ratingSchema, 'Ratings')
 
-app.on('error', (err, ctx) => {
-  log.error('server error', err)
-})
-
-// post request for api
-_.post('/Score', async (ctx) => {
-  const name = ctx.request.body.name;
+// POST
+_.post('/rate', async (ctx) => {
   const date = ctx.request.body.date;
   const rating = ctx.request.body.rating;
-  const comment = ctx.request.body.rating;
-  ctx.query = {
-    name: String,
-    date: Date,
-    rating: Number,
-    comment: String
-  }
+  const comment = ctx.request.body.comment;
 
-  if (!name || !date || !rating || !comment) {
-    console.log('please fill in data')
-  } else {
-    const steve = new Rating({ name: name, date: date, rating: rating, comment: comment })
-    steve.save(function (err, steve) {
-      if (err) return console.error(err);
-    });
-  }
-  console.log(ctx.request.body)
-  ctx.body = JSON.stringify(ctx.request.body);
+  const steve = new Rating({ date: date, rating: rating, comment: comment })
+  steve.save(function (err, steve) {
+    if (err) return console.error(err);
+  });
 })
 
-// get request for api
-_.get('/Day', async (ctx) => {
+// GET
+_.get('/rating', async (ctx) => {
   Rating.find(function (err, Rating) {
     if (err) return console.error(err);
     console.log(Rating)
+    ctx.body = JSON.stringify(ctx.request.body);
   })
 })
 
-// put request for api
+// PUT
 _.put(`/update`, async (ctx) => {
   let doc = await Rating.findOne({ date: '2020-03-17' });
   await Rating.updateOne({ date: '2020-03-17' }, { date: '2021-03-17' })
   await doc.save();
 })
 
+// DELETE
 _.delete('/delete', async (ctx) => {
   Rating.deleteMany({ __v: 0 }, function (err) {
     if (err) return handleError(err);
