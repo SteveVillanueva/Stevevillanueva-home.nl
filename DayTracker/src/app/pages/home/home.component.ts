bf10fdeea62297e97643d83b3ec24eaa0e9050cb
Steve Villanueva <steve.villanueva@edu-kw1c.nl>
2020-05-07 14:36:55 +0200

    Generates random data for home page

--- DayTracker/src/app/pages/home/home.component.ts
+++ DayTracker/src/app/pages/home/home.component.ts
@@ -9,26 +9,61 @@ import { RatingService } from '../rating.service';
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent implements OnInit {
-  RatingGet: Array<Ratings>;
-  RatingPost: PostRating = {
+  RatingGet: Array<Ratings> = [];
+  RatingPost: Ratings = {
     date: new Date(),
     rating: null,
     mood: '',
     comment: ''
   };
+  mockData: Ratings;
+
   constructor(public rate: RatingService) { }
 
   ngOnInit(): void {
-    this.GetRating();
+
+
+    this.generateRatings();
   }
+  randomDate(start, end) {
+    return new Date(start.getTime() + Math.random() * (end.getTime() - start.getTime()));
+  }
+
+  makeRandom(lengthOfCode: number, possible: string) {
+    let text = '';
+    for (let i = 0; i < lengthOfCode; i++) {
+      text += possible.charAt(Math.floor(Math.random() * possible.length));
+    }
+    return text;
+  }
+
+
+
+  generateRatings() {
+    let possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890,./;'[]\=-)(*&^%$#@!~`";
+    const moods = ['Angry', 'Anxious', 'Hyper', 'Calm', 'Sad', 'Motivated', 'Happy', 'Tired', 'Stressed', 'Neutral'];
+    for (let i = 0; i <= 50 - 1; i++) {
+      this.mockData = {
+        date: this.randomDate(new Date(2020, 0, 1), new Date()),
+        rating: Math.floor(Math.random() * 10),
+        mood: moods[Math.floor(Math.random() * moods.length)],
+        comment: this.makeRandom(50, possible)
+      };
+      this.RatingGet.push(this.mockData);
+    }
+    console.log(this.RatingGet);
+    return this.RatingGet;
+  }
+
   GetRating(): void {
     this.rate.getRatings().subscribe(
       data => {
         this.RatingGet = data,
           // tslint:disable-next-line: only-arrow-functions
-          this.RatingGet.map(function (RatingGet) {
-            RatingGet.date = new Date(RatingGet.date);
-          });
+          console.log(this.RatingGet)
+        this.RatingGet.map(function (RatingGet) {
+          RatingGet.date = new Date(RatingGet.date);
+        });
       }
     );
   }
